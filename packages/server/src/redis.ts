9e6ba54145d4888f4c60cfb043ac7455b2c1b73c
ben awad <benawad97@gmail.com>
2018-06-23 10:38:33 -0500

    dokku

--- packages/server/src/redis.ts
+++ packages/server/src/redis.ts
@@ -1,3 +1,6 @@
 import * as Redis from "ioredis";
 
-export const redis = new Redis();
+export const redis =
+  process.env.NODE_ENV === "production"
+    ? new Redis(process.env.REDIS_URL)
+    : new Redis();
--- packages/server/src/utils/createTypeormConn.ts
+++ packages/server/src/utils/createTypeormConn.ts
@@ -2,5 +2,11 @@ import { getConnectionOptions, createConnection } from "typeorm";
 
 export const createTypeormConn = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-  return createConnection({ ...connectionOptions, name: "default" });
+  return process.env.NODE_ENV === "production"
+    ? createConnection({
+        ...connectionOptions,
+        url: process.env.DATABASE_URL,
+        name: "default"
+      } as any)
+    : createConnection({ ...connectionOptions, name: "default" });
 };
