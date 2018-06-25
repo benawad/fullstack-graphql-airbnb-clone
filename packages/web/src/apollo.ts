6c746091bd5c386354567425290ae69fc3f71fa9
ben awad <benawad97@gmail.com>
2018-06-25 14:34:04 -0500

    netlify

--- packages/web/src/apollo.ts
+++ packages/web/src/apollo.ts
@@ -1,10 +1,9 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
-
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:4000",
+    uri: process.env.REACT_APP_SERVER_URL,
     credentials: "include"
   }),
   cache: new InMemoryCache()
