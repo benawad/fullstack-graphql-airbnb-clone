9f1adca72ee6b6470f382b935218ebdf6c7b2aec
Rao Venugopal <rao.venugopal@adroll.com>
2018-07-07 01:23:17 -0700

    Minor change to ensure web and app are consistent with respect to each other

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -1,12 +1,13 @@
-import * as React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import * as React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-import { RegisterConnector } from "../modules/register/RegisterConnector";
-import { LoginConnector } from "../modules/login/LoginConnector";
+import { RegisterConnector } from '../modules/register/RegisterConnector';
+import { LoginConnector } from '../modules/login/LoginConnector';
 
 export const Routes = () => (
   <BrowserRouter>
     <Switch>
+      <Redirect from="/" exact={true} to="/login" />
       <Route exact={true} path="/register" component={RegisterConnector} />
       <Route exact={true} path="/login" component={LoginConnector} />
     </Switch>
