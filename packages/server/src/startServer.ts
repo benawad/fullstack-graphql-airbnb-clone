c3c35e7d9962ea109e553211299d75feb56df46b
Ben Awad <benawad97@gmail.com>
2018-08-07 22:40:19 -0500

    setup subscriptions in react

--- packages/server/src/startServer.ts
+++ packages/server/src/startServer.ts
@@ -1,13 +1,14 @@
 import "reflect-metadata";
 // tslint:disable-next-line:no-var-requires
 require("dotenv-safe").config();
-import { GraphQLServer, PubSub } from "graphql-yoga";
+import { GraphQLServer } from "graphql-yoga";
 import * as session from "express-session";
 import * as connectRedis from "connect-redis";
 import * as RateLimit from "express-rate-limit";
 import * as RateLimitRedisStore from "rate-limit-redis";
 import { applyMiddleware } from "graphql-middleware";
 import * as express from "express";
+import { RedisPubSub } from "graphql-redis-subscriptions";
 
 import { redis } from "./redis";
 import { createTypeormConn } from "./utils/createTypeormConn";
@@ -30,7 +31,7 @@ export const startServer = async () => {
   const schema = genSchema() as any;
   applyMiddleware(schema, middleware);
 
-  const pubsub = new PubSub();
+  const pubsub = new RedisPubSub();
 
   const server = new GraphQLServer({
     schema,
--- packages/web/src/apollo.ts
+++ packages/web/src/apollo.ts
@@ -1,11 +1,34 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createUploadLink } from "apollo-upload-client";
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 
-const link = createUploadLink({
+const httpLink = createUploadLink({
   uri: process.env.REACT_APP_SERVER_URL,
   credentials: "include"
 });
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/`,
+  options: {
+    reconnect: true
+  }
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query) as any;
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
 
 export const client = new ApolloClient({
   link,
