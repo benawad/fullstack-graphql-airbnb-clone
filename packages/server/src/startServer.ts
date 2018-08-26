07b92779fd7c90339737d2de4b0556fa18c84958
Ben Awad <benawad97@gmail.com>
2018-08-26 09:30:54 -0500

    secure false

--- packages/server/src/startServer.ts
+++ packages/server/src/startServer.ts
@@ -75,7 +75,8 @@ export const startServer = async () => {
       saveUninitialized: false,
       cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        // secure: process.env.NODE_ENV === "production",
+        secure: false,
         maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
       }
     } as any)
