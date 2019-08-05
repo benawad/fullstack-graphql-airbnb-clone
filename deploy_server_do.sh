a742340dc68f6ba62d33c54f8f6af35a0196b42d
surzycka <asurzyck@indiana.edu>
2019-08-05 13:08:06 -0400

    Update deploy_server_do.sh

--- deploy_server_do.sh
+++ deploy_server_do.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 yarn build:server
-heroku container:push --app=calm-citadel-25445 web
-heroku container:release --app=calm-citadel-25445 web
+heroku container:push --app=murmuring-brook-12282 web
+heroku container:release --app=murmuring-brook-12282 web
 # docker build -t benawad/abb:latest .
 # docker push benawad/abb:latest
 # ssh root@167.99.11.233 "docker pull benawad/abb:latest && docker tag benawad/abb:latest dokku/abb:latest && dokku tags:deploy abb latest"
